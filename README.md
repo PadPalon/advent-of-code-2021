f00b0ba722733508e3220035b9ce34c01ea8bff9
Luca Neukom <luca.neukom@gmail.com>
2021-12-02 21:25:07 +0100

    prepare java development

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advent-of-code-2021
+
 Solving puzzles from https://adventofcode.com/2021
+
+One folder per programming language in case I get bored of one.
---
+++ java/src/main/java/ch/neukom/advent2021/helper/InputResourceReader.java
@@ -0,0 +1,43 @@
+package ch.neukom.advent2021.helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class InputResourceReader implements AutoCloseable {
+    private final Class<?> clazz;
+
+    private BufferedReader reader;
+
+    public InputResourceReader(Class<?> clazz) {
+        this.clazz = clazz;
+    }
+
+    public Stream<String> readInput() {
+        InputStream is = clazz.getResourceAsStream("input");
+        if (is == null) {
+            System.out.println("Could not read input");
+            return Stream.empty();
+        }
+        InputStreamReader isr = new InputStreamReader(is);
+        reader = new BufferedReader(isr);
+        return reader.lines();
+    }
+
+    public String getFirstLine() {
+        return readInput().findFirst().orElseThrow();
+    }
+
+    public long getLineCount() {
+        return readInput().count();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (reader != null) {
+            reader.close();
+        }
+    }
+}
